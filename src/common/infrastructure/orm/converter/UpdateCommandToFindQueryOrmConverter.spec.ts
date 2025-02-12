8b4d81375a57d581cd94d9dabc0882e7ab80540d
Adrianmjim <adrianmjim199434@gmail.com>
2025-02-12 23:44:39 +0100

    Add first test

---
+++ src/common/infrastructure/orm/converter/UpdateCommandToFindQueryOrmConverter.spec.ts
@@ -0,0 +1,52 @@
+import { beforeAll, describe, expect, it, Mocked, vitest } from 'vitest';
+
+import { SQL } from 'drizzle-orm';
+
+import { UpdateCommandToFindQueryOrmConverter } from './UpdateCommandToFindQueryOrmConverter';
+import { Converter } from '../../../domain/converter/Converter';
+import { Query } from '../../../domain/model/Query';
+import { UpdateCommand } from '../../../domain/model/UpdateCommand';
+
+describe(UpdateCommandToFindQueryOrmConverter.name, () => {
+  let findQueryToFindQueryOrmConverterMock: Mocked<Converter<Query, SQL | undefined>>;
+  let updateCommandToFindQueryOrmConverter: UpdateCommandToFindQueryOrmConverter<UpdateCommand, SQL | undefined>;
+
+  beforeAll(() => {
+    findQueryToFindQueryOrmConverterMock = {
+      convert: vitest.fn(),
+    };
+
+    updateCommandToFindQueryOrmConverter = new UpdateCommandToFindQueryOrmConverter(
+      findQueryToFindQueryOrmConverterMock,
+    );
+  });
+
+  describe('.convert()', () => {
+    describe('when called', () => {
+      let updateCommandFixture: UpdateCommand
+      let queryOrmFixture: SQL | undefined;
+      let result: unknown;
+
+      beforeAll(() => {
+        updateCommandFixture = {
+          findQuery: {},
+          setCommand: {},
+        };
+        queryOrmFixture = undefined;
+
+        findQueryToFindQueryOrmConverterMock.convert.mockReturnValueOnce(queryOrmFixture);
+
+        result = updateCommandToFindQueryOrmConverter.convert(updateCommandFixture);
+      });
+
+      it('should call findQueryToFindQueryOrmConverter.convert()', () => {
+        expect(findQueryToFindQueryOrmConverterMock.convert).toHaveBeenCalledTimes(1);
+        expect(findQueryToFindQueryOrmConverterMock.convert).toHaveBeenCalledWith(updateCommandFixture.findQuery);
+      });
+
+      it('should return SQL | undefined', () => {
+        expect(result).toBe(queryOrmFixture);
+      });
+    });
+  });
+});
